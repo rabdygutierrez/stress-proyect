b9cef7f58f336f66029e819057b876173d3e4434
Rabdy Gutierrez <rabdygutierrez@gmail.com>
2025-05-20 22:15:44 -0400

    Type_Test2

--- scripts/smoke-test.js
+++ scripts/smoke-test.js
@@ -8,18 +8,7 @@ const vusGauge = new Gauge('active_vus');
 // Tomamos el valor de la variable de entorno TYPE_TEST (p. ej. “loadTest”)
 const typeTestTag = __ENV.TYPE_TEST || 'default';
 
-export const options = {
-  vus: 1,                // Número de usuarios virtuales
-  duration: '1m',        // Duración de la prueba: 1 minuto
-  thresholds: {
-    http_req_failed: ['rate<0.01'],    // Tasa de errores HTTP mínima
-    http_req_duration: ['p(95)<500'],  // 95% de las solicitudes < 500 ms
-  },
-  // Añadimos tags globales: estos tags viajan con cada métrica hacia InfluxDB
-  tags: {
-    type_test: typeTestTag
-  }
-};
+export let options = TypesTest.typesTest[__ENV.TYPE_TEST]
 
 export default function () {
   // Registramos cuántos VUs hay activos en este instante
