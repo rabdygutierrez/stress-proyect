d0e6ed57ebe1b93747880ac40bdeb9fccbe791f7
Rabdy Gutierrez <rabdygutierrez@gmail.com>
2025-06-21 17:12:38 -0400

    update compose

--- k6/scripts/scenarios/k6_login-compra.js
+++ k6/scripts/scenarios/k6_login-compra.js
@@ -153,6 +153,14 @@ export default function () {
     }));
   sleep(1);
 
+// Capturar el token y guardarlo en el archivo de salida
+const tokensFilePath = '/k6/output/tokens.json';  // Ruta donde se guardará el archivo de tokens
+const tokens = JSON.parse(open(tokensFilePath) || '[]');  // Leer el archivo, si no existe inicializa como un array vacío
+tokens.push({ token });  // Agregar el nuevo token al array
+
+// Escribir los tokens en el archivo de salida
+writeFileSync(tokensFilePath, JSON.stringify(tokens, null, 2));  // Guardar los tokens en formato JSON en el archivo
+
   // --- liveSession ---
   const livePayload = JSON.stringify({
     token: user_access_token,
